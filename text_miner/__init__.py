0e9fd9aa8d1d31641a69b87cb455e8714d030cbb
Jose Varona <jovalab92@gmail.com>
2023-03-28 13:49:46 -0400

    Not importing default functions

--- text_miner/__init__.py
+++ text_miner/__init__.py
@@ -1,3 +1,3 @@
-from .text_extractor import extract_text_from_file
-from .text_processor import LanguageProcessor, DocumentContainer, detect_language
-from .vector_space_model import VectorSpaceModel
+# from .text_extractor import extract_text_from_file
+# from .text_processor import LanguageProcessor, DocumentContainer, detect_language
+# from .vector_space_model import VectorSpaceModel
